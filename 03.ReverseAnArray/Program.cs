68bf38cbd22c91afc943e56e3aeb7df581a605b1
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-10-17 22:28:17 +0300

    arrays simple

---
+++ 03.ReverseAnArray/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _03.ReverseAnArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read the array (n lines of integers)
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+                arr[i] = int.Parse(Console.ReadLine());
+
+            // Print the elements from the last to the first
+            for (int i = n - 1; i >= 0; i--)
+                Console.Write(arr[i] + " ");
+
+        }
+    }
+}
