5dd2d28ba565267e0e0766f350727102a9a1b7bc
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-10-24 22:42:39 +0300

    lists

--- 27.ZigZagArrays/Program.cs
+++ 27.ZigZagArrays/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> outputList1 = new List<int>();
+            List<int> outputList2 = new List<int>();
+
+            int rows = int.Parse(Console.ReadLine() ?? "1");
+
+            for (int i = 0; i < rows; i++)
+            {
+                List<int> currentInputList = (Console.ReadLine() ?? "1").Split(" ").Select(int.Parse).ToList();
+                if (i % 2 != 0)
+                {
+                    outputList1.Insert(i, currentInputList[0]);
+                    outputList2.Insert(i, currentInputList[1]);
+                }
+                else
+                {
+                    outputList1.Insert(i, currentInputList[1]);
+                    outputList2.Insert(i, currentInputList[0]);
+                }
+
+                currentInputList = new List<int>();
+            }
+            Console.WriteLine(string.Join(" ", outputList2));
+            Console.WriteLine(string.Join(" ", outputList1));
         }
     }
 }
---
+++ 28.ArrayRotation/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _28.ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputList = (Console.ReadLine() ?? "1").Split(" ").Select(int.Parse).ToList();
+            int rotationsCount = int.Parse(Console.ReadLine()?? "1");
+
+            for (int i = 0; i < rotationsCount; i++)
+            {
+                int currentNum = inputList[0];
+                inputList.RemoveAt(0);
+                inputList.Add(currentNum);
+            }
+
+            //for (int i = 1; i <= inputList.Count; i++)
+            //{
+            //    while (rotationsCount > 0)
+            //    {
+            //        int temp = inputList[0];
+
+            //        inputList.RemoveAt(0);
+            //        inputList.Add(temp);
+            //        rotationsCount -- ;
+            //        temp = 0;
+            //    }
+
+
+            //}
+
+            Console.WriteLine(string.Join(" ", inputList));
+        }
+    }
+}
