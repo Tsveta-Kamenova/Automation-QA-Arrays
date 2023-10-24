017ba763eaff6983871bcd958e80816063f63ee8
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-10-24 21:42:30 +0300

    more lists

--- 25.BombNumbers/Program.cs
+++ 25.BombNumbers/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> inputList1 = (Console.ReadLine() ?? "1").Split(" ").Select(int.Parse).ToList();
+            List<int> bombList = (Console.ReadLine() ?? "1").Split(" ").Select(int.Parse).ToList();
+
+            int bombNum = bombList[0];
+            int bombPower = bombList[1];
+
+            for (int i = 0; i < inputList1.Count; i++)
+            {
+                if (inputList1[i] == bombNum)
+                {
+                    int startIndex = i - bombPower;
+                    int removeCount = bombPower + bombPower + 1;
+
+                    if (startIndex < 0)
+                    {
+                        startIndex = 0;
+                    }
+
+                    if (startIndex + removeCount > inputList1.Count)
+                    {
+                        removeCount = inputList1.Count - startIndex;
+                    }
+
+                    inputList1.RemoveRange(startIndex, removeCount);
+                    i = startIndex - 1;
+                }
+            }
+            //Console.WriteLine(string.Join(" ", inputList1));
+            int sum = inputList1.Sum();
+            Console.WriteLine(sum);
         }
     }
 }
